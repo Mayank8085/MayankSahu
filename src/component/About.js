4a999f8522409d9f2ef2b8b176bc624a9b8e90ed
Mayank8085 <mayanksahu1806@gmail.com>
2022-02-28 21:58:48 +0530

    remume updated

--- src/component/About.js
+++ src/component/About.js
@@ -1,4 +1,3 @@
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import aboutImage from "../assets/images/about.png";
@@ -8,34 +7,36 @@ import Skills from "./Skills";
 const About = () => {
   return (
     <>
-    <div className="container mt-5 py-5" id="about">
-      <div className="row">
-        <div className="col-md-6 text-center col-12">
-          <img src={aboutImage} alt="about" className="img-fluid" />
-        </div>
-        <div className="col-md-6 col-12 text-center mt-4">
-          <h1 className="text-center fs-1 bolder">About Me</h1>
-          <p className="fs-5 text-center p-3 mb-4">
-            👋I'm Mayank Sahu, from Pune, India. A self-taught developer. I'm
-            passionate about web development and am constantly eager to learn
-            new technologies. I'm currently working on something exciting
-            and looking to collaborate on Web related projects especially involving MERN Stack.
-          </p>
-          <button
-            type="button"
-            class="btn text-white px-3 text-center rounded   neu-div"
-            onClick={() => {
-              window.open(Resume, "_blank");
-            }}
-          >
-            <h4 className=" font-weight-bold">Download Resume
-            <FontAwesomeIcon icon="fa-solid fa-download" className="px-2" />
-            </h4>
-          </button>
+      <div className="container mt-5 py-5" id="about">
+        <div className="row">
+          <div className="col-md-6 text-center col-12">
+            <img src={aboutImage} alt="about" className="img-fluid" />
+          </div>
+          <div className="col-md-6 col-12 text-center mt-4">
+            <h1 className="text-center fs-1 bolder">About Me</h1>
+            <p className="fs-5 text-center p-3 mb-4">
+              👋I'm Mayank Sahu, from Pune, India. A self-taught developer. I'm
+              passionate about web development and am eager to learn about new
+              technologies . I'm currently working on an exciting project and
+              looking forward to collaborate on Web related projects especially
+              involving MERN Stack.
+            </p>
+            <button
+              type="button"
+              class="btn text-white px-3 text-center rounded   neu-div"
+              onClick={() => {
+                window.open(Resume, "_blank");
+              }}
+            >
+              <h4 className=" font-weight-bold">
+                Download Resume
+                <FontAwesomeIcon icon="fa-solid fa-download" className="px-2" />
+              </h4>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-    <Skills/>
+      <Skills />
     </>
   );
 };
